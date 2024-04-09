1878568c296935e91d7d7e124c2f4bedaa06fe74
sashaj03 <148829995+sashaj03@users.noreply.github.com>
2024-04-09 16:53:15 -0700

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,42 @@
+class Node implements Comparable<Node> {
+
+		private char val;
+		private int freq;
+		
+		Node right;
+		Node left;
+
+		public Node(char value, int frequency) {
+
+			this.val = value;
+			this.freq = frequency;
+			
+		}
+		
+		public char getVal() {
+			return this.val;
+		}
+		
+		public int getFreq() {
+			return this.freq;
+		}
+		
+		public void setRight(Node node) {
+			this.right = node;
+		}
+		
+		public void setLeft(Node node) {
+			this.left = node;
+		}
+
+
+		public String toString() {
+			return "" + this.val;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return Integer.compare(this.getFreq(), o.getFreq());
+		}
+	}
