5286b4c8dc4f7dfb3dbcd7e6074a669d6b838e05
Claire Lodermeier <clairelodermeier@Claires-MacBook-Pro.local>
2024-04-09 21:43:10 -0700

    encoderTest

--- EncoderTest.java
+++ EncoderTest.java
@@ -14,7 +14,16 @@ class EncoderTest {
 
 	@Test
 	void testAbc() throws IOException {
-		//Encoder enc = new Encoder("abc.txt");
+		Encoder enc = new Encoder("abc.txt");
+	}
+	
+	@Test
+	void testYoga() throws IOException {
+		Encoder enc = new Encoder("yoga.txt");
 	}
 
+	@Test
+	void testAusten() throws IOException {
+		Encoder enc = new Encoder("austen.txt");
+	}
 }
